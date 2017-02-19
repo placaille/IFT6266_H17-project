1ece9c533260b0cd6977fc2cc3d3348922cbd222
Philippe Lacaille <philippe.lacaille@gmail.com>
2017-02-19 17:21:23 -0500

    ajust save func

--- code/mscoco_inpainting.py
+++ code/mscoco_inpainting.py
@@ -46,13 +46,13 @@ def get_batch_data(batch_idx,
     with open(caption_path) as fd:
         caption_dict = pkl.load(fd)
 
-    print data_path + "/*.jpg"
+    # print data_path + "/*.jpg"
     imgs = np.asarray(glob.glob(data_path + "/*.jpg"))
     batch_imgs = imgs[batch_idx]
 
     color_count = 0
-    inputs = np.array([])
-    targets = np.array([])
+    inputs = np.ndarray((batch_size, 64, 64, 3)).astype(theano.config.floatX)
+    targets = np.ndarray((batch_size, 32, 32, 3)).astype(theano.config.floatX)
     captions = {}
     for i, img_path in enumerate(batch_imgs):
         img = Image.open(img_path)
@@ -73,11 +73,16 @@ def get_batch_data(batch_idx,
         else:  # if black and white (do nothing)
             continue
 
-        inputs = np.append(inputs, input)
-        targets = np.append(targets, target)
+        inputs[color_count - 1] = input
+        targets[color_count - 1] = target
         captions[color_count - 1] = caption_dict[cap_id]
 
-    return inputs, targets, captions, color_count
+    returns = [inputs[:color_count] / 255,
+               targets[:color_count] / 255,
+               captions,
+               color_count]
+
+    return returns
 
 
 def init_dataset(args, dataset_name):
@@ -120,7 +125,7 @@ def build_network(input_var=None):
     weight_init = lasagne.init.Normal()
 
     # encoder
-    conv1_nb_filt = 64
+    conv1_nb_filt = 32
     conv1_sz_filt = (9, 9)
     conv1_sz_padd = 2
     # conv1 output size = (60, 60)
@@ -128,7 +133,7 @@ def build_network(input_var=None):
     pool1_sz = (2, 2)
     # pool1 output size = (30, 30)
 
-    conv2_nb_filt = 128
+    conv2_nb_filt = 32
     conv2_sz_filt = (7, 7)
     conv2_sz_padd = 0
     # conv2 output size = (24, 24)
@@ -136,7 +141,7 @@ def build_network(input_var=None):
     pool2_sz = (4, 4)
     # pool2 size = (6, 6)
 
-    conv3_nb_filt = 256
+    conv3_nb_filt = 32
     conv3_sz_filt = (5, 5)
     conv3_sz_padd = 0
     # conv3 output size = (2, 2)
@@ -148,7 +153,7 @@ def build_network(input_var=None):
     # dense1 output to the decoder (vector 512)
 
     # decoder
-    tconv1_nb_filt = 256
+    tconv1_nb_filt = 32
     tconv1_sz_filt = (5, 5)
     tconv1_sz_strd = (1, 1)
     # conv1 output size = (5, 5)
@@ -156,7 +161,7 @@ def build_network(input_var=None):
     upsamp1_sz = (2, 2)
     # upsamp1 output size = (10, 10)
 
-    tconv2_nb_filt = 128
+    tconv2_nb_filt = 32
     tconv2_sz_filt = (4, 4)
     tconv2_sz_strd = (1, 1)
     # tconv2 output size = (13, 13)
@@ -164,7 +169,7 @@ def build_network(input_var=None):
     upsamp2_sz = (2, 2)
     # upsamp2 output size = (26, 26)
 
-    tconv3_nb_filt = 64
+    tconv3_nb_filt = 32
     tconv3_sz_filt = (5, 5)
     tconv3_sz_strd = (1, 1)
     # tconv3 output size = (30, 30)
@@ -247,6 +252,181 @@ def build_network(input_var=None):
 
     return network
 
+
+def build_small_network(input_var=None):
+    """
+    Build the network using Lasagne library
+    """
+
+    ##################
+    # Network config #
+    ##################
+
+    input_channels = 3
+    weight_init = lasagne.init.GlorotUniform()
+
+    # encoder
+    conv1_nb_filt = 32
+    conv1_sz_filt = (9, 9)
+    conv1_sz_padd = 2
+    # conv1 output size = (32, 60, 60)
+
+    pool1_sz = (2, 2)
+    # pool1 output size = (32, 30, 30)
+
+    conv2_nb_filt = 32
+    conv2_sz_filt = (7, 7)
+    conv2_sz_padd = 0
+    # conv2 output size = (32, 24, 24)
+
+    pool2_sz = (4, 4)
+    # pool2 size = (32, 6, 6)
+
+    conv3_nb_filt = 32
+    conv3_sz_filt = (5, 5)
+    conv3_sz_padd = 0
+    # conv3 output size = (32, 2, 2)
+
+    pool3_sz = (2, 2)
+    # pool3 output size = (32, 1, 1)
+
+    dens1_nb_unit = 512
+    # dense1 output to the decoder (vector 512)
+
+    # decoder
+    tconv1_nb_filt = 32
+    tconv1_sz_filt = (5, 5)
+    tconv1_sz_strd = (1, 1)
+    # conv1 output size = (5, 5)
+
+    upsamp1_sz = (2, 2)
+    # upsamp1 output size = (10, 10)
+
+    tconv2_nb_filt = 32
+    tconv2_sz_filt = (4, 4)
+    tconv2_sz_strd = (1, 1)
+    # tconv2 output size = (13, 13)
+
+    upsamp2_sz = (2, 2)
+    # upsamp2 output size = (26, 26)
+
+    tconv3_nb_filt = 32
+    tconv3_sz_filt = (5, 5)
+    tconv3_sz_strd = (1, 1)
+    # tconv3 output size = (30, 30)
+
+    tconv4_nb_filt = 3
+    tconv4_sz_filt = (3, 3)
+    tconv4_sz_strd = (1, 1)
+    # tconv4 output size = (32, 32)
+
+    # final output = (3 channels, 32 x 32)
+
+    #####################
+    # Build the network #
+    #####################
+
+    # Add input layer
+    network = lyr.InputLayer(
+        shape=(None, input_channels, 64, 64), input_var=input_var)
+
+    # Add convolution layer
+    network = lyr.Conv2DLayer(incoming=network, num_filters=conv1_nb_filt,
+                              filter_size=conv1_sz_filt, pad=conv1_sz_padd,
+                              W=weight_init)
+    # Add pooling layer
+    network = lyr.MaxPool2DLayer(incoming=network, pool_size=pool1_sz)
+
+    # Add convolution layer
+    network = lyr.Conv2DLayer(incoming=network, num_filters=conv2_nb_filt,
+                              filter_size=conv2_sz_filt, pad=conv2_sz_padd,
+                              W=weight_init)
+    # Add pooling layer
+    network = lyr.MaxPool2DLayer(incoming=network, pool_size=pool2_sz)
+
+    # Add convolution layer
+    network = lyr.Conv2DLayer(incoming=network, num_filters=conv3_nb_filt,
+                              filter_size=conv3_sz_filt, pad=conv3_sz_padd,
+                              W=weight_init)
+    # Add pooling layer
+    network = lyr.MaxPool2DLayer(incoming=network, pool_size=pool3_sz)
+
+    network = lyr.FlattenLayer(network)
+
+    # Add dense layer
+    network = lyr.DenseLayer(network, dens1_nb_unit, W=weight_init)
+    network = lyr.ReshapeLayer(
+        network, (input_var.shape[0], dens1_nb_unit, 1, 1))
+
+    # Add transposed convolution layer
+    network = lyr.TransposedConv2DLayer(incoming=network,
+                                        num_filters=tconv1_nb_filt,
+                                        filter_size=tconv1_sz_filt,
+                                        stride=tconv1_sz_strd,
+                                        W=weight_init)
+    # Add upsampling layer
+    network = lyr.Upscale2DLayer(incoming=network, scale_factor=upsamp1_sz)
+
+    # Add transposed convolution layer
+    network = lyr.TransposedConv2DLayer(incoming=network,
+                                        num_filters=tconv2_nb_filt,
+                                        filter_size=tconv2_sz_filt,
+                                        stride=tconv2_sz_strd,
+                                        W=weight_init)
+    # Add upsampling layer
+    network = lyr.Upscale2DLayer(incoming=network, scale_factor=upsamp2_sz)
+
+    # Add transposed convolution layer
+    network = lyr.TransposedConv2DLayer(incoming=network,
+                                        num_filters=tconv3_nb_filt,
+                                        filter_size=tconv3_sz_filt,
+                                        stride=tconv3_sz_strd,
+                                        W=weight_init)
+
+    # Add transposed convolution layer
+    network = lyr.TransposedConv2DLayer(incoming=network,
+                                        num_filters=tconv4_nb_filt,
+                                        filter_size=tconv4_sz_filt,
+                                        stride=tconv4_sz_strd,
+                                        W=weight_init,
+                                        nonlinearity=lasagne.nonlinearities.sigmoid)
+
+    return network
+
+
+def gen_pics(inputs, targts, preds, epoch, show=False, save=False):
+    """
+    Generates and/or save image out of array using PIL
+    """
+    if save or show:
+
+        i = 0
+        for input, targt, pred in zip(inputs, targts, preds):
+
+            i += 1
+            # set contour so we can fill the middle part with true and prediction
+            true_im = np.copy(input)
+            pred_im = np.copy(input)
+
+            center = (int(np.floor(true_im.shape[0] / 2.)), int(np.floor(true_im.shape[1] / 2.)))
+
+            true_im[center[0] - 16:center[0] + 16, center[1] - 16:center[1] + 16, :] = targt
+            pred_im[center[0] - 16:center[0] + 16, center[1] - 16:center[1] + 16, :] = pred
+
+            true_im = Image.fromarray(np.uint8(true_im * 255))
+            pred_im = Image.fromarray(np.uint8(pred_im * 255))
+
+            if save:
+
+                true_im.save('./output/img_epoch_%s_id_%s_true.jpg' % (epoch + 1, i))
+                pred_im.save('./output/img_epoch_%s_id_%s_pred.jpg' % (epoch + 1, i))
+
+            if show:
+
+                true_im.show(title='img_epoch_%s_id_%s_true' % (epoch + 1, i))
+                pred_im.show(title='img_epoch_%s_id_%s_pred' % (epoch + 1, i))
+
+
 if __name__ == '__main__':
 
     args = get_args()
@@ -258,25 +438,25 @@ if __name__ == '__main__':
 
     # Setup network, params and updates
     network = build_network(input_var=input_data.dimshuffle(0, 3, 1, 2))
-    output = lyr.get_output(network).dimshuffle(0, 2, 3, 1)
+    preds = lyr.get_output(network).dimshuffle(0, 2, 3, 1)
     loss = T.mean(lasagne.objectives.squared_error(
-        output, targt_data))
-    preds = output
+        preds, targt_data))
 
     params = network.get_params(trainable=True)
     updates = lasagne.updates.adam(loss, params, learning_rate=0.01)
 
     # Compile Theano functions
     print 'compiling...'
-    train = theano.function(inputs=[input_data, targt_data], outputs=[
-                            loss, output], updates=updates)
+    train = theano.function(inputs=[input_data, targt_data],
+                            outputs=loss, updates=updates)
     print '- train compiled.'
-    # valid = theano.function(inputs=[input_idx], outputs=[loss, preds])
+    valid = theano.function(inputs=[input_data, targt_data],
+                            outputs=[loss, preds])
     print '- valid compiled.'
     print 'compiled.'
 
     batch_size = 256
-    nb_epochs = 200
+    nb_epochs = 30
     early_stop_limit = 10
     NB_TRAIN = 82782
     NB_VALID = 40504
@@ -290,57 +470,85 @@ if __name__ == '__main__':
     for i in xrange(nb_epochs):
 
         # iterate over minibatches for training
-        schemes = ShuffledScheme(examples=NB_TRAIN,
-                                 batch_size=batch_size)
+        schemes_train = ShuffledScheme(examples=NB_TRAIN,
+                                       batch_size=batch_size)
 
         epoch_acc = 0
         epoch_loss = 0
         num_batch = 0
 
-        for batch_idx in schemes.get_request_iterator():
+        print 'Epoch #%s of %s' % ((i + 1), nb_epochs)
+
+        for batch_idx in schemes_train.get_request_iterator():
 
             # get training data for this batch
             inputs, targts, capts, color_count = get_batch_data(
                 batch_idx, mscoco=dataset_path, split="train2014")
 
-            inputs_rsp = np.reshape(inputs, (color_count, 64, 64, 3)) / 255
-            targts_rsp = np.reshape(targts, (color_count, 32, 32, 3)) / 255
+            batch_loss = train(inputs, targts)
 
-            print 'inputs shape:\t', inputs_rsp.shape
-            print 'targts shape:\t', targts_rsp.shape
+            if num_batch % 100 == 0:
+                print '- train batch %s, loss %s' % (num_batch, np.round(batch_loss, 4))
 
-            inputs_rsp = inputs_rsp.astype(theano.config.floatX)
-            targts_rsp = targts_rsp.astype(theano.config.floatX)
-
-            batch_loss, output = train(inputs_rsp, targts_rsp)
-
-            print 'outpts shape:\t', output.shape
             epoch_loss += batch_loss
             num_batch += 1
 
-        # train_acc.append(np.round(epoch_acc / num_batch, 4))
-        train_loss.append(np.round(epoch_loss / num_batch, 4))
 
-        epoch_loss, preds = valid(valid_idx)
-        # valid_acc.append(np.round(batch_acc, 4))
-        valid_loss.append(np.round(epoch_loss, 4))
+            ID_PRINT = [1111, 2222, 3333]
+            inputs_print, targts_print, capts_print, color_count_print = get_batch_data(
+                ID_PRINT, mscoco=dataset_path, split='val2014')
 
-        print 'Epoch #%s of %s' % ((i + 1), nb_epochs)
-        print '- Train (loss %s)' % (train_loss[i])
-        print '- Valid (loss %s)' % (valid_loss[i])
+            _, preds_print = valid(inputs_print, targts_print)
 
-        if valid_loss[i] < best_valid_loss:
-            best_valid_loss = valid_loss[i]
-            best_epoch_idx = i
-            early_stp_counter = 0
-        else:
-            early_stp_counter += 1
-            if early_stp_counter >= early_stop_limit:
-                print '**Early stopping activated, %s epochs without improvement.' % early_stop_limit
-                break
+            gen_pics(inputs_print, targts_print, preds_print, i, save=True)
 
-        plt.imsave(fname='img_epoch_%s_.jpg' %
-                   (i + 1), arr=preds[0][0], cmap='gray')
+
+        train_loss.append(np.round(epoch_loss, 4))
+
+        print '- Epoch train (loss %s)' % (train_loss[i])
+
+        # # iterate over minibatches for valid
+        # schemes_valid = ShuffledScheme(examples=NB_VALID,
+        #                                batch_size=batch_size)
+        #
+        # num_batch = 0
+        #
+        # for batch_idx in schemes_valid.get_request_iterator():
+        #
+        #     # get training data for this batch
+        #     inputs, targts, capts, color_count = get_batch_data(
+        #         batch_idx, mscoco=dataset_path, split="val2014")
+        #
+        #     batch_loss, preds = valid(inputs, targts)
+        #
+        #     if num_batch % 100 == 0:
+        #         print '- valid batch %s, loss %s' % (num_batch, np.round(batch_loss, 4))
+        #
+        #     epoch_loss += batch_loss
+        #     num_batch += 1
+        #
+        # valid_loss.append(np.round(epoch_loss, 4))
+        #
+        # print '- Epoch valid (loss %s)' % (valid_loss[i])
+        #
+        # if valid_loss[i] < best_valid_loss:
+        #     best_valid_loss = valid_loss[i]
+        #     best_epoch_idx = i
+        #     early_stp_counter = 0
+        # else:
+        #     early_stp_counter += 1
+        #     if early_stp_counter >= early_stop_limit:
+        #         print '**Early stopping activated, %s epochs without improvement.' % early_stop_limit
+        #         break
+
+        # Generate images
+        ID_PRINT = [1111, 2222, 3333]
+        inputs_print, targts_print, capts_print, color_count_print = get_batch_data(
+            ID_PRINT, mscoco=dataset_path, split='val2014')
+
+        _, preds_print = valid(inputs_print, targts_print)
+
+        gen_pics(inputs_print, targts_print, i, save=True)
 
     print 'Training completed.'
 
